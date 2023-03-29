d8aa5fe7c86c2fc5b47c94b3d07843e7b21615ed
Rahul <developer.rahul9888@gmail.com>
2023-03-30 01:56:26 +0530

    some changes

--- app/Http/Controllers/Shop/ShopController.php
+++ app/Http/Controllers/Shop/ShopController.php
@@ -359,18 +359,18 @@ class ShopController extends Controller
     public function checkout(Request $request)
     {   
 
-        $user = auth()->user();
-        $level = 1;
-        $count = $this->customerRepository->getUserTeamLevel($user->id,$level);
-        $upgradeLevel = pow(2,$level);
-        if($count >= $upgradeLevel && $user->user_level <= $level+1) {
-            echo 'Rahul';
-            $this->customerRepository->updateHoldIncome($user->id,$level+1);
-            $this->upgradeAccountToTop($user->id,$level+1);
-            $user->user_level = $level + 2;
-            $user->save();
-        }
-        die;
+        // $user = auth()->user();
+        // $level = 1;
+        // $count = $this->customerRepository->getUserTeamLevel($user->id,$level);
+        // $upgradeLevel = pow(2,$level);
+        // if($count >= $upgradeLevel && $user->user_level <= $level+1) {
+        //     echo 'Rahul';
+        //     $this->customerRepository->updateHoldIncome($user->id,$level+1);
+        //     $this->upgradeAccountToTop($user->id,$level+1);
+        //     $user->user_level = $level + 2;
+        //     $user->save();
+        // }
+        // die;
         // $this->customerRepository->storeResponse();
         // $incomes = $this->customerRepository->getTempIncome();
 
